a4c336ea765c0ace70a3df940808e6d92fee39c9
ventosae <anton.surov@hotmail.com>
2018-09-21 19:10:26 +1000

    Module2 Task1 beaty fixes

--- js/stat.js
+++ js/stat.js
@@ -14,6 +14,8 @@ var HORIZONTAL_POSITION = 60;
 var VERTICAL_POSITION = 40;
 var RESULTS_VERTICAL = 5;
 var NAME_VERTICAL = 20;
+var FONT = '16px PT Mono';
+var FONT_COLOR = '#000';
 
 // paint cloud
 var renderCloud = function (ctx, x, y, color) {
@@ -33,25 +35,20 @@ var getMaxElement = function (arr) {
 };
 
 // write text message
-var writeMessage = function (ctx, message, color, font, x, y) {
-  ctx.font = font;
-  ctx.fillStyle = color;
+var writeMessage = function (ctx, message, x, y) {
+  ctx.font = FONT;
+  ctx.fillStyle = FONT_COLOR;
   ctx.fillText(message, CLOUD_X + x, CLOUD_Y + y);
 };
 
 var getColumnColor = function (ctx, names) {
-  if (names === 'Вы') {
-    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  } else {
-    ctx.fillStyle = 'rgba(43, 92, 252, ' + Math.random() + ')';
-  }
+  ctx.fillStyle = names === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'rgba(43, 92, 252, ' + Math.random() + ')';
 };
 
-
 var renderHistogram = function (ctx, names, times) {
+  var columnY = CLOUD_Y + CLOUD_HEIGHT - VERTICAL_POSITION;
   for (var i = 0; i < names.length; i++) {
     var columnX = CLOUD_X + HORIZONTAL_POSITION + (COLUMN_GAP + COLUMN_WIDTH) * i;
-    var columnY = CLOUD_Y + CLOUD_HEIGHT - VERTICAL_POSITION;
     var columnHeight = (Math.round(times[i]) * HISTOGRAM_HEIGHT) / getMaxElement(times);
     ctx.fillStyle = getColumnColor(ctx, names[i]);
     ctx.fillRect(columnX, columnY, COLUMN_WIDTH, -columnHeight);
@@ -67,8 +64,8 @@ window.renderStatistics = function (ctx, names, times) {
   // paint cloud
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
   // writing message
-  writeMessage(ctx, TEXT_1, '#000', '16px PT Mono', 60, 30);
-  writeMessage(ctx, TEXT_2, '#000', '16px PT Mono', 60, 50);
+  writeMessage(ctx, TEXT_1, 60, 30);
+  writeMessage(ctx, TEXT_2, 60, 50);
   // paint histogram
   renderHistogram(ctx, names, times);
 };
