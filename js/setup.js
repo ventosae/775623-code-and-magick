4841611ca9cbb30918d31a30c40a26b0675767ce
ventosae <anton.surov@hotmail.com>
2018-09-23 18:14:14 +1000

    Module3 Task 1 - Refactoring and Fixes

--- js/setup.js
+++ js/setup.js
@@ -3,56 +3,48 @@
 var NUMBER_OF_WITCHERS = 4;
 
 // Создаем массив характеристик
-var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+// Копируем шаблон, задаем лист и создаем фрагмент
+var witchersTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var fragment = document.createDocumentFragment();
+var withchersList = document.querySelector('.setup-similar-list');
 
 // Пишем рандомизатор
-var getRandomElement = function (variable) {
-  return variable[Math.floor(Math.random() * (variable.length))];
-};
-
-// Убераем скрывающие классы
-var removeClass = function (element, classToRemove) {
-  document.querySelector(element).classList.remove(classToRemove);
+var getRandomElement = function (arrayName) {
+  return arrayName[Math.floor(Math.random() * (arrayName.length))];
 };
 
 // Создаем случайного волшебника
 var randomWitcher = function () {
   var witcher = {};
-  witcher.name = getRandomElement(names) + ' ' + getRandomElement(surnames);
-  witcher.coatColor = getRandomElement(coatColor);
-  witcher.eyesColor = getRandomElement(eyesColor);
+  witcher.name = getRandomElement(NAMES) + ' ' + getRandomElement(SURNAMES);
+  witcher.coatColors = getRandomElement(COAT_COLORS);
+  witcher.eyesColors = getRandomElement(EYE_COLORS);
   return witcher;
 };
 
 // Добовляем DOM элемент на основании объекта
 var createWitcher = function (witcherData) {
-  var fragment = document.createDocumentFragment();
-  var witchersTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
   var withchersInfo = witchersTemplate.cloneNode(true);
   withchersInfo.querySelector('.setup-similar-label').textContent = witcherData.name;
-  withchersInfo.querySelector('.wizard-coat').style.fill = witcherData.coatColor;
-  withchersInfo.querySelector('.wizard-eyes').style.fill = witcherData.eyesColor;
+  withchersInfo.querySelector('.wizard-coat').style.fill = witcherData.coatColors;
+  withchersInfo.querySelector('.wizard-eyes').style.fill = witcherData.eyesColors;
   return fragment.appendChild(withchersInfo);
 };
 
-// Делаем функцию заполнения блока DOM элементами
-var appendWitcher = function (witcher) {
-  var withchersList = document.querySelector('.setup-similar-list');
-  withchersList.appendChild(witcher);
-};
-
 // Создаем и добовляем волшебников
-var addWitchers = function (witcherNumber) {
-  removeClass('.setup', 'hidden');
-  for (var i = 0; i < witcherNumber; i++) {
+var addWitchers = function () {
+  document.querySelector('.setup').classList.remove('hidden');
+  for (var i = 0; i < NUMBER_OF_WITCHERS; i++) {
     var witcherElements = randomWitcher();
     var witcherDom = createWitcher(witcherElements);
-    appendWitcher(witcherDom);
+    withchersList.appendChild(witcherDom);
   }
-  removeClass('.setup-similar', 'hidden');
+  document.querySelector('.setup-similar').classList.remove('hidden');
 };
 
-addWitchers(NUMBER_OF_WITCHERS);
+addWitchers();
