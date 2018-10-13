f678691494c6b27828cae758f59c3f62cba85510
ventosae <anton.surov@hotmail.com>
2018-10-13 14:34:57 +1000

    make character selection window draggable

--- js/setup.js
+++ js/setup.js
@@ -86,6 +86,7 @@ setupWindowOpen.addEventListener('click', function () {
 
 setupWindowClose.addEventListener('click', function () {
   addHidden(setupWindow);
+  setupWindow.removeAttribute('style');
 });
 
 setupWindowOpen.addEventListener('keydown', function (evt) {
@@ -112,8 +113,63 @@ setupWitcherBall.addEventListener('click', function () {
   changeBackgroundColor(styleWitcherBall, FIREBALL_COLORS, valueWitcherBall);
 });
 
+// модуль прередвигания диалога персонажа
+(function () {
+  var setupDialogElement = document.querySelector('.setup');
+  var dialogHandler = setupDialogElement.querySelector('.upload');
+
+  dialogHandler.addEventListener('mousedown', function (evt) {
+    evt.preventDefault(); // зачем?
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setupDialogElement.style.top = (setupDialogElement.offsetTop - shift.y) + 'px';
+      setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + 'px';
+
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (evnt) {
+          evnt.preventDefault();
+          dialogHandler.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogHandler.addEventListener('click', onClickPreventDefault);
+      }
+
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+})();
+
 // Создаем и добовляем волшебников
-var addWitchers = function () {
+(function () {
 
   for (var i = 0; i < NUMBER_OF_WITCHERS; i++) {
     var witcherElements = randomWitcher();
@@ -121,6 +177,6 @@ var addWitchers = function () {
     withchersList.appendChild(witcherDom);
   }
   document.querySelector('.setup-similar').classList.remove('hidden');
-};
-addWitchers();
+}());
+
 
